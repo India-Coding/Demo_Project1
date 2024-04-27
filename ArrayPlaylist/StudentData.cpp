df761803b56c95fe45f89a0f26b5d78dfbabfa2b
vivek <jk408mp@gmail.com>
2024-04-27 16:33:23 +0530

    Student data display using array

---
+++ ArrayPlaylist/StudentData.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string stu[5]={"Ram","Gyan","Sonu"};
+    cout<<'Student data Display using array';
+    for(int i=0;i<5;i++)
+    {
+        cout<<stu[i];
+    }
+    return 0;
+}
