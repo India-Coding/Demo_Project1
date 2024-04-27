544d736364a3b66c598042480fbe5901efbd97ad
vivek <jk408mp@gmail.com>
2024-04-27 18:15:41 +0530

    problem in sizeof operator

---
+++ ArrayPlaylist/ProblemInSizeofOptr.cpp
@@ -0,0 +1,10 @@
+//Question:-function using array
+#include<iostream>
+using namespace std;
+int main()
+{
+int arr[10]={1,2,3,4};
+int arrsize=sizeof(arr)/sizeof(int);
+cout<<"Calculate array size="<<arrsize;
+   return 0;
+}
